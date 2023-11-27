4552a09c9623e1f1d16db8f609a35a266e52e849
Instrutor <gilberto.braga0021@gmail.com>
2023-11-27 16:14:00 -0300

    - Commit do dia 27

---
+++ divisao.py
@@ -0,0 +1,15 @@
+# Calculadora Simples
+# Divisão
+
+# Apresentação
+print('\n\t\t\t -- Divisão --\n')
+
+# Entrada
+n1 = float(input('Informe o valor1: '))
+n2 = float(input('Informe o valor2: '))
+
+# Processamento
+q = n1//n2 # q - quociente
+
+# Saída
+print(f'{n1} div {n2} = {q}')
